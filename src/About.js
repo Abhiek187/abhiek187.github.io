2b911ebcc851192a31c9e34c98385c59c86b5f66
Abhiek187 <achaudhuri2011@yahoo.com>
2019-01-09 21:15:20 -0500

    Make minor changes
    Offline functionality is currently not working due to problems with routing to other projects.

--- src/About.js
+++ src/About.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class About extends Component {
 	render() {
-    const bio = "Abhishek Chaudhuri is a college student from North Brunswick, New Jersey. He studies" +
-      " at Rutgers University-New Brunswick, majors in Computer Engineering, and minors in Computer" +
-      " Science. He excels in subjects like math and science and has a strong penchant for" +
-      " programming and technology. Some of his hobbies include doing taekwondo, playing the flute &" +
-      " piccolo, and playing video games. His ultimate goal in life is to help make society better" +
-      " for everyone. He can put his technical skills to good use by gaining experience from working" +
-      " with other companies who value his ambitions.";
+    const bio = 'Abhishek Chaudhuri is a college student from North Brunswick, New Jersey. He studies' +
+      ' at Rutgers University-New Brunswick, majors in Computer Engineering, and minors in Computer' +
+      ' Science. He excels in subjects like math and science and has a strong penchant for' +
+      ' programming and technology. Some of his hobbies include doing taekwondo, playing the flute &' +
+      ' piccolo, and playing video games. His ultimate goal in life is to help make society better' +
+      ' for everyone. He can put his technical skills to good use by gaining experience from working' +
+      ' with other companies who value his ambitions.';
     document.getElementsByTagName('title')[0].textContent = 'Abhishek Chaudhuri - About';
 
 		return (
