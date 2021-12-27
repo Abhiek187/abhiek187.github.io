9bc8d620e8f1afaddf4b44f840cacf2c9c8c1f46
Abhiek187 <achaudhuri2011@yahoo.com>
2021-12-26 23:50:44 -0500

    Implement horizontal scroll buttons for the projects lists

--- src/tsx/Projects.tsx
+++ src/tsx/Projects.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
 
 import "../scss/Projects.scss";
 import projectData from "../models/projects.json";
-import { OnClickProp } from "./App";
+import { Direction, OnClickProp } from "./App";
 import ProjectDetails from "./ProjectDetails";
 import ProjectError from "./ProjectError";
 
@@ -31,16 +31,32 @@ export const capitalize = (str: string): string =>
   str.charAt(0).toUpperCase() + str.slice(1);
 
 const Projects: React.FC<ProjectsProps> = ({ onClickLink, isDarkMode }) => {
+  // path is the URL relative to the parent (/projects), while URL is the entire URL
+  const { path, url } = useRouteMatch();
+  // Set the type of the imported JSON
+  const projects = projectData as ProjectsJSON;
+  // Save a reference to each project list
+  const projectsListRef = useRef<(HTMLUListElement | null)[]>(
+    Array(Object.keys(projects).length)
+  );
+
   useEffect(() => {
     document.title = "Abhishek Chaudhuri - Projects";
     document.querySelector(".links-about")?.classList.remove("active");
     document.querySelector(".links-projects")?.classList.add("active");
     document.querySelector(".links-contact")?.classList.remove("active");
   }, []);
-  // path is the URL relative to the parent (/projects), while URL is the entire URL
-  const { path, url } = useRouteMatch();
-  // Set the type of the imported JSON
-  const projects = projectData as ProjectsJSON;
+
+  const scrollList = (index: number, direction: Direction) => {
+    const { current: projectsLists } = projectsListRef;
+
+    // The cards have a width of 300px + 8px * 2 of margin
+    projectsLists[index]?.scrollBy({
+      top: 0,
+      left: direction === Direction.Right ? 316 : -316,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <main className="projects container-fluid">
@@ -58,7 +74,7 @@ const Projects: React.FC<ProjectsProps> = ({ onClickLink, isDarkMode }) => {
         <Switch>
           <Route exact path={path}>
             <ul className="projects-full-list">
-              {(Object.keys(projects) as [ProjectTypes]).map((type) => (
+              {(Object.keys(projects) as [ProjectTypes]).map((type, index) => (
                 /* Each list item needs a key */
                 <li key={type} className="projects-type-list">
                   {/* Apple does what others don't */}
@@ -72,11 +88,15 @@ const Projects: React.FC<ProjectsProps> = ({ onClickLink, isDarkMode }) => {
                       className={`projects-scroll-left btn ${
                         isDarkMode ? "btn-info" : "btn-primary"
                       }`}
+                      onClick={() => scrollList(index, Direction.Left)}
                       aria-label={`Scroll ${type} projects left`}
                     >
                       <i className="fas fa-arrow-left" />
                     </button>
-                    <ul className="projects-list">
+                    <ul
+                      className="projects-list"
+                      ref={(el) => (projectsListRef.current[index] = el)}
+                    >
                       {projects[type].map((project) => (
                         <li
                           key={project.id}
@@ -120,6 +140,7 @@ const Projects: React.FC<ProjectsProps> = ({ onClickLink, isDarkMode }) => {
                       className={`projects-scroll-right btn ${
                         isDarkMode ? "btn-info" : "btn-primary"
                       }`}
+                      onClick={() => scrollList(index, Direction.Right)}
                       aria-label={`Scroll ${type} projects right`}
                     >
                       <i className="fas fa-arrow-right" />
