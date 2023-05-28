8e9e13c951eeda93a2e35e5995229886abb647ad
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2023-05-28 16:59:11 -0400

    Use Vite's PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +17,39 @@ export default defineConfig({
       "node-fetch": "isomorphic-fetch",
     },
   },
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "Abhishek Chaudhuri",
+        name: "Abhishek Chaudhuri - Portfolio Website",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "icon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        start_url: "/",
+        display: "minimal-ui",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
